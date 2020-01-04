ef6edfb3e91e7d79b30198ad52cca7ce2c17d1f7
Emre Ozan <mremreozan@gmail.com>
2020-01-05 00:25:49 +0100

    list functions

--- zzz Python Applications/Hackerrank/999 lists.py
+++ zzz Python Applications/Hackerrank/999 lists.py
@@ -24,3 +24,28 @@ if __name__ == '__main__':
             result = sorted(result)
         if command == 'remove':
             result.remove(int(x[1]))
+            
+'''
+Input (stdin)
+
+12
+insert 0 5
+insert 1 10
+insert 0 6
+print
+remove 6
+append 9
+append 1
+sort
+print
+pop
+reverse
+print
+
+
+Your Output (stdout)
+[6, 5, 10]
+[1, 5, 9, 10]
+[9, 5, 1]
+
+'''
