8ae928fff86d7cd045f3dd457e2d85a05a5e4855
Emre Ozan <mremreozan@gmail.com>
2020-01-11 00:36:55 +0100

    Cartesian

---
+++ zzz Python Applications/Hackerrank/987 itertools.product().py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 11 00:16:01 2020
+
+@author: Emre Ozan
+"""
+
+from itertools import product
+
+if __name__ == '__main__':
+    n = [int(i) for i in input().split()]
+    m = [int(i) for i in input().split()]
+    print(*product(n, m))         # direct results
+    print(list(product(n, m)))    # as a list
+
+
+'''
+input:
+1 2
+
+3 4
+
+output:
+(1, 3) (1, 4) (2, 3) (2, 4)
+[(1, 3), (1, 4), (2, 3), (2, 4)]
+
+'''
