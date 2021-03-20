14e059ad73baae2390b43af2392576f670c03852
becode <mremreozan@gmail.com>
2021-03-20 08:24:19 +0100

    the last changes before internship

---
+++ Data Warehouse/Google Cloud Platform (GCP)/989_/training/project_env.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+# Create the DEVSHELL_PROJECT_ID on a VM
+curl "http://metadata.google.internal/computeMetadata/v1/project/project-id" -H "Metadata-Flavor: Google" > Projec$
+awk '{print "export DEVSHELL_PROJECT_ID=" $0, "\n" "export BUCKET=" $0, "\n" "export JAVA_HOME=/usr/lib/jvm/java-8$
+source env.txt
+echo $DEVSHELL_PROJECT_ID
